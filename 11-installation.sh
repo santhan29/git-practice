c7017b908444f0fbf41d22ce827fe8dbd9fb207f
santhan29 <santhanpeddaguravagari@gmail.com>
2024-08-27 18:19:52 +0530

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 userid=$(id -u)
-echo "user id is $userid"
+#echo "user id is $userid"
+
+if [ $userid -ne 0 ]
+    then 
+        echo "please run script with root privileges"
+fi 
+
+dnf install git -y 
