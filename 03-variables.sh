88bb3f6ef55cdedf0f12383e48151d958931c0b8
santhan29 <santhanpeddaguravagari@gmail.com>
2024-08-27 13:16:10 +0530

    i added new text in variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1=ramesh
+person2=suresh
+
+
+echo "$person1: hi ${person2} how are you"
+echo "${person2}: hi $person1 i am fine, how do you do"
+echo "$person1: i am doing good ${person2} whats going on"
+echo "${person2}: i am learining devops"
