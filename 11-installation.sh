0d45217a30bcaa16a46900871cd7bdbe280cca2c
santhan29 <santhanpeddaguravagari@gmail.com>
2024-08-27 19:22:39 +0530

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# userid=$(id -u)
-# #echo "user id is $userid"
+userid=$(id -u)
+#echo "user id is $userid"
 
-# if [ $userid -ne 0 ]
-#     then 
-#         echo "please run script with root privileges"
-#         exit 1
-# fi 
+if [ $userid -ne 0 ]
+    then 
+        echo "please run script with root privileges"
+        exit 1
+fi 
 
-# dnf list installed git 
+dnf list installed git 
 
-# if [ $? -ne 0 ]
-# then
-#     echo "git is not installed then install git"
-#     dnf install git -y 
-#     if [ $? -ne 0 ]
-#     then 
-#         echo "git installation not success"
-#         exit 1
-#     else 
-#         echo "git installation is success"
-#     fi 
-# else 
-#     echo "git already installed nothing to do"
-# fi 
+if [ $? -ne 0 ]
+then
+    echo "git is not installed then install git"
+    dnf install git -y 
+    if [ $? -ne 0 ]
+    then 
+        echo "git installation not success"
+        exit 1
+    else 
+        echo "git installation is success"
+    fi 
+else 
+    echo "git already installed nothing to do"
+fi 
 
 dnf list installed mysql
 
---
+++ 12-functions.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+userid=$(id -u)
+#echo "user id is $userid"
+
+VALIDATE(){
+    echo "exit status: $1"
+}
+
+if [ $userid -ne 0 ]
+    then 
+        echo "please run script with root privileges"
+        exit 1
+fi 
+
+dnf list installed git 
+
+VALIDATE $?
+
+# if [ $? -ne 0 ]
+# then
+#     echo "git is not installed then install git"
+#     dnf install git -y 
+#     if [ $? -ne 0 ]
+#     then 
+#         echo "git installation not success"
+#         exit 1
+#     else 
+#         echo "git installation is success"
+#     fi 
+# else 
+#     echo "git already installed nothing to do"
+# fi 
+
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+# then
+#     echo "mysql is not installed install it"
+#     dnf install mysql -y 
+#     if [ $? -ne 0 ]
+#     then 
+#         echo "mysql installation is failure please check"
+#         exit 1 
+#     else 
+#         echo "mysql installation is success"
+#     fi 
+# else 
+#     echo "mysql is already installed nothing to do"
+# fi 
+
